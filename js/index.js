17a19855a2f559158f33254bc5d72747c0266f6d
Odiphri <odiphrimarvellous@gmail.comm>
2026-05-01 19:54:42 +0100

    fifth commit

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@
 function showSignup() {
   document.getElementById("login-form").style.display = "none";
   document.getElementById("register-form").style.display = "block";
+  document.getElementById("login-tab").classList.remove("active");
+  document.getElementById("register-tab").classList.add("active");
 }
 
 function showLogin() {
   document.getElementById("register-form").style.display = "none";
   document.getElementById("login-form").style.display = "block";
+  document.getElementById("register-tab").classList.remove("active");
+  document.getElementById("login-tab").classList.add("active");
 }
